56b8b716a78c25f86f2511a1897c84d432ebe597
kangyeop.lee <kangyeop.lee@linecorp.com>
2025-12-01 14:37:08 +0900

    feat: update product detail page

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainLayout from './components/layout/main-layout'
 import HomePage from './pages/home'
 import ProductListPage from './pages/product-list'
-import ProductDetailPage from './pages/product-detail'
 import ProductFormPage from './pages/product-form'
 import OrderListPage from './pages/order-list'
 import OrderFormPage from './pages/order-form'
@@ -18,8 +17,7 @@ function App() {
             <Route index element={<HomePage />} />
             <Route path="products" element={<ProductListPage />} />
             <Route path="products/new" element={<ProductFormPage />} />
-            <Route path="products/:id" element={<ProductDetailPage />} />
-            <Route path="products/:id/edit" element={<ProductFormPage />} />
+            <Route path="products/:id" element={<ProductFormPage />} />
             <Route path="orders" element={<OrderListPage />} />
             <Route path="orders/new" element={<OrderFormPage />} />
             <Route path="orders/:id" element={<OrderFormPage />} />
